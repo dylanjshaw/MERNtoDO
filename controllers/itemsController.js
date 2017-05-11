6a5a0b32ed88e65df8798ec179cf28b3bc5bb257
dylanjshaw <dylanjshaw@sandiego.edu>
2017-05-11 12:28:14 -0700

    render item via its id

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,17 +1,40 @@
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Items = require('../models/items');
 
-
 module.exports = function(app) {
+
+  // app.use(bodyParser.json());
+  // app.use(bodyParser.urlencoded({ extended: true }));
 
   app.get('/', function(req, res){
     res.send('whats up world');
   });
+  //landing page
 
-  // app.get('/items/all', function(req, res){
-  //   mongoose.model('items').find(function(err, items){
-  //     res.send(items);
-  //   });
-  // });
+  app.get('/items/:name', function(req, res){
+    Items.find({user: req.params.name}, function(err, items){
+      res.send(items);
+    });
+  });
+  //send all items associated with user
 
+  app.get('/items/all', function(req, res){
+    Items.find(function(err, items){
+      res.send(items);
+    });
+  });
+  //send all items
+
+  app.get('/item/:id', function(req, res){
+    Items.findById(req.params.id, function(err, item){
+      res.send(item);
+    });
+  });
 }
+  //send an item via its ID
+
+    // Items.findById({_id: req.params.id}, function(err, item){
+    //   if (err) throw err;
+    //   res.send(item);
+    // });
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,11 +1,8 @@
-var mongoose = require('mongoose');
 var Users = require('../models/users');
-
-
 module.exports = function(app) {
 
   app.get('/users/all', function(req, res){
-    mongoose.model('Users').find(function(err, users){
+    Users.find(function(err, users){
       res.send(users);
     });
   });
